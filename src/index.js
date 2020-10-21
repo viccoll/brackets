c4bf30ac8cbbf778ad29cc740da99bea283e1c2d
Victoria Gabrielchik <victoria.gabrielchik@yandex.com>
2020-10-21 21:53:37 +0300

    feat: add check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  bracketsConfig.forEach(el => arr.push(el[0].concat(el[1])));
+  let tstr = str, a, m;
+  for (let i = 0; i < arr.length; i++) {
+      m = arr[i]; a = tstr.replace(m, ''); tstr = a;
+  }
+  if (tstr === str)  return false;
+  else if (tstr === '')  return true;
+  else return check(tstr, bracketsConfig);
 }
